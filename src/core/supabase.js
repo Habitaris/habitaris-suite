575002c5b25d0bee536a2473822ecd1d03d741b7
Habitaris <habitaris@outlook.com>
2026-02-25 11:06:32 -0500

    fix: update Supabase API key

--- src/core/supabase.js
+++ src/core/supabase.js
@@ -5,7 +5,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://xlzkasdskatnikuavefh.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsemthc2Rzc2thdG5pa3VhdmVmaCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzQwMTUyOTk3LCJleHAiOjIwNTU3Mjg5OTd9.DP5x1hNbnTSzIFRMFOG7tYbykaAJMc6BRXYC_dFNFgE";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsemthc2Rza2F0bmlrdWF2ZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4OTE3NzQsImV4cCI6MjA4NzQ2Nzc3NH0.SR9tIpvL0YnV9CNrRq4T-xetifuNQOJZE0OnQpwtYLM";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_KEY);
 export { SUPABASE_URL, SUPABASE_KEY };
