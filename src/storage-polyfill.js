121da1252c938e8be5c6e26a82a977f57604f303
Habitaris <habitaris@outlook.com>
2026-02-24 23:28:13 -0500

    Fix: correct Supabase API key in storage-polyfill

--- src/storage-polyfill.js
+++ src/storage-polyfill.js
@@ -11,7 +11,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SB_URL = "https://xlzkasdskatnikuavefh.supabase.co";
-const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsemthc2Rzc2thdG5pa3VhdmVmaCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzQwMTUyOTk3LCJleHAiOjIwNTU3Mjg5OTd9.DP5x1hNbnTSzIFRMFOG7tYbykaAJMc6BRXYC_dFNFgE";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhsemthc2Rza2F0bmlrdWF2ZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4OTE3NzQsImV4cCI6MjA4NzQ2Nzc3NH0.SR9tIpvL0YnV9CNrRq4T-xetifuNQOJZE0OnQpwtYLM";
 
 const sb = createClient(SB_URL, SB_KEY);
 
